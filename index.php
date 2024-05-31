0b46cebfe3ea9b79f4293e574d4a071a71e29e4f
deshhnajain <deshjain03@gmail.com>
2024-05-31 20:01:04 +0530

    Initial commit

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
 
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "paw-foundation";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-
-}
-?>
 
 <!DOCTYPE html>
 <html lang="en">

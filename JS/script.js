1a70adc9c1fc7fbde758b47341afa207bf694dd4
deshhnajain <deshjain03@gmail.com>
2024-06-03 01:44:08 +0530

    Initial Commit

--- JS/script.js
+++ JS/script.js
@@ -15,34 +15,31 @@ function carousel() {
         headerss = 1;
     }
     x[headerss - 1].style.display = "block";
-    setTimeout(carousel, 1000);
+    setTimeout(carousel, 2000); // Adjust the timing as needed
 }
 
 // Automatic Slideshow for About >> Mission and Vision
-var cardss = 0;
+var cardIndex = 0;
 showSlides();
 
 function showSlides() {
     var i;
-    var c1 = document.getElementsByClassName("card");
-    var c2 = document.getElementsByClassName("card-2");
-    for (i = 0; i < c1.length; i++) {
-        c1[i].style.display = "none";
+    var cards1 = document.getElementsByClassName("card");
+    var cards2 = document.getElementsByClassName("card-2");
+    
+    for (i = 0; i < cards1.length; i++) {
+        cards1[i].style.display = "none";
     }
-    cardss++;
-    if (cardss > c1.length) {
-        cardss = 1;
+    for (i = 0; i < cards2.length; i++) {
+        cards2[i].style.display = "none";
     }
-    for (i = 0; i < c2.length; i++) {
-        c2[i].style.display = "none";
+    cardIndex++;
+    if (cardIndex > cards1.length) {
+        cardIndex = 1;
     }
-    cardss++;
-    if (cardss > c2.length) {
-        cardss = 1;
-    }
-    c1[cardss - 1].style.display = "block";
-    c2[cardss - 1].style.display = "block";
-    setTimeout(showSlides, 1000);
+    cards1[cardIndex - 1].style.display = "block";
+    cards2[cardIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Adjust the timing as needed
 }
 
 // Signup form sidebar functionality
@@ -54,12 +51,12 @@ document.getElementById("sidebarButton").onclick = function (event) {
         sidebar.style.width = "50%";
     }
     event.stopPropagation();
+};
 
 document.addEventListener("click", function (event) {
     var sidebar = document.getElementById("sidebar");
     var sidebarButton = document.getElementById("sidebarButton");
 
-    // Check if the sidebar is open and the click is outside the sidebar and the button
     if (sidebar.style.width === "50%" && !sidebar.contains(event.target) && !sidebarButton.contains(event.target)) {
         sidebar.style.width = "0";
     }
